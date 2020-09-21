04baa7e56755ecd23bf4ff220064154dcc9b2e56
sean <229224551@qq.com>
2020-09-21 21:08:13 +0800

    douban_demo

--- shoppingMall/lib/core/service/config.dart
+++
@@ -1,9 +0,0 @@
-
-class HttpConfig{
-  static const String baseURL = "http://152.136.185.210:8000/api/z8";
-  static const int timeout = 5000;
-}
-
-class HomeConfig{
-  static const String homeUrl = "/home/data";
-}
